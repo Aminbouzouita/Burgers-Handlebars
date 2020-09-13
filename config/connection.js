e3ca6ed5d652fd673c4610d357104a227e325d39
Amin Bouzouita <Aminbouzouita@yahoo.com>
2020-09-12 21:30:29 -0700

    update connection parameter

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "w1h4cr5sb73o944p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Anna032118!",
-  database: "burgers_db"
+  user: "u8lmbhx0ugdjinqu",
+  password: "	odst3txynxftwt9z",
+  database: "nuln45nigecdyz6y"
 });
 
 connection.connect(function(err) {
